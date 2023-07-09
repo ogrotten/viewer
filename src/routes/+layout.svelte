75cb578f62b9a4d38a51f958fcda309e3a0eb7c4
Darrin Lowery <darrin@buildlab.co>
2023-07-09 13:13:25 -0500

    navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import AuthCheck from '$lib/components/AuthCheck.svelte'
 	import '../app.css'
+	import TopNav from './_components/TopNav.svelte'
 </script>
 
-<slot />
+<TopNav />
+<div class="p-6 m-4 mt-0 border-t-2 border-gray-700">
+	<slot />
+</div>
---
+++ src/routes/_components/TopNav.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { dbUser } from '$lib/firestore'
+	// import { goto } from '$app/navigation'
+	// import { auth } from '$lib/firebase'
+	// import { onMount } from 'svelte'
+	// // import type { PageData } from './$types'
+
+	// export let data: PageData
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<!-- <a class="text-xl normal-case btn btn-ghost">daisyUI</a> -->
+		<ul class="px-1 menu menu-horizontal">
+			{#if $dbUser}
+				<li><a href="/manage">Manage</a></li>
+				<li><a href="/show">Show</a></li>
+				<!-- <li><a>Link</a></li>
+			<li><a>Link</a></li> -->
+			{/if}
+		</ul>
+	</div>
+	<div class="flex-none">
+		<ul class="px-1 menu menu-horizontal">
+			{#if $dbUser}
+				<li><a href="/profile">Profile</a></li>
+				<li><a href="/logout">Logout</a></li>
+			{:else}
+				<li><a href="/login">Login</a></li>
+			{/if}
+			<!-- <li><a>Link</a></li>
+			<li><a>Link</a></li> -->
+		</ul>
+	</div>
+</div>
