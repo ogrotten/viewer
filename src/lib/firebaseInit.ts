18174056e865a90271017c18a4c6fce0618d8e14
Darrin Lowery <darrin@buildlab.co>
2023-07-09 01:11:59 -0500

    clientside fb

---
+++ src/lib/firebaseInit.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app"
+// import { getAnalytics } from "firebase/analytics"
+import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyC1sH0nkn8vM0rhC47i2pOU77FAco3ljFg",
+	authDomain: "viewer-54075.firebaseapp.com",
+	projectId: "viewer-54075",
+	storageBucket: "viewer-54075.appspot.com",
+	messagingSenderId: "548385998993",
+	appId: "1:548385998993:web:f0237c7a405215e29b9730",
+	measurementId: "G-8FQQB400R2"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app)
+const db = getFirestore(app)
+const auth = getAuth(app)
