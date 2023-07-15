05a6f8458ce8e5ba70eddd7b546bd3442ec76cc7
Darrin Lowery <darrin@buildlab.co>
2023-07-14 20:25:51 -0500

    gallery snapshots!

--- src/routes/show/+page.svelte
+++ src/routes/show/+page.svelte
@@ -9,7 +9,27 @@
 		getDocs,
 		deleteDoc,
 		updateDoc,
+		where,
 	} from 'firebase/firestore'
 	import { db } from '$lib/firebase'
 	import { dbUser } from '$lib/firestore'
+
+	let gallery: Image[]
+
+	let unsubGallery
+
+	async function getGallery() {
+		const q = query(
+			collection(db, 'viewers', $dbUser?.uid, 'images'),
+			where('gallery', '==', true),
+		)
+
+		unsubGallery = onSnapshot(q, snap => {
+			gallery = [...snap.docs].map(doc => ({ id: doc.id, ...doc.data() }))
+		})
+	}
+
+	$: if ($dbUser?.id) getGallery()
+
+	$: console.log(`LOG..+page: gallery`, gallery)
 </script>
