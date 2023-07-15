5e06ef169ded838871d156f737efc73248076418
Darrin Lowery <darrin@buildlab.co>
2023-07-15 11:25:03 -0500

    configure FB hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto'
+import adapterStatic from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +12,21 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
 
-export default config;
+		// adapter: adapter({
+		// 	pages: "build",
+		// 	assets: "build",
+		// 	fallback: "index.html",
+		// 	precompress: true,
+		// 	strict: true
+		// })
+
+		adapter: adapterStatic({
+			pages: './.firebase/public',
+			assets: './.firebase/public',
+			fallback: 'index.html',
+		}),
+	}
+}
+
+export default config
