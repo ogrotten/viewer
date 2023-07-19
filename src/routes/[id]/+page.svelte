1eec6662d2d7fff6dce116ef7a9ebdf1d4b29cfd
Darrin Lowery <darrin@buildlab.co>
2023-07-19 17:36:33 -0500

    add many

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { fly } from 'svelte/transition'
 	import { onMount } from 'svelte'
 	import {
 		addDoc,
@@ -25,11 +26,14 @@
 			id: '',
 		},
 		urlValid = false,
-		disableNow = false
+		many = '',
+		manyFiltered: Image[],
+		google = ''
 
 	let show = { gallery: false, now: false, carousel: false },
 		tiles = false,
-		tab = 0
+		tab = 1,
+		disableNow = false
 
 	const resetImage = () => {
 		newImg = {
@@ -57,35 +61,24 @@
 
 	const addOne = async (incoming: Image) => {
 		// Add a new document with a generated id.
-		console.log(`LOG..+page: addOne`, incoming)
 		const docRef = await addDoc(collection(db, `viewers/${$dbUser?.uid}/images`), incoming)
 		getImages()
 	}
 
-	function handleInputChange(e: Event & { target: HTMLInputElement; code: string }) {
-		const regex = new RegExp(
-			/^(ftp|http|https|chrome|:\/\/|\.|@){2,}(localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\S*:\w*@)*([a-zA-Z]|(\d{1,3}|\.){7}){1,}(\w|\.{2,}|\.[a-zA-Z]{2,3}|\/|\?|&|:\d|@|=|\/|\(.*\)|#|-|%)*$/gmu,
-		)
-		let item: Image = {
-			url: e.target.value,
-			carousel: false,
-			gallery: false,
-			now: false,
-			title: '',
-		}
-
-		urlValid = regex.test(item.url)
+	const addMany = () => {
+		manyFiltered.forEach(async (image, idx) => {
+			await addDoc(collection(db, `viewers/${$dbUser?.uid}/images`), image)
+		})
+		getImages()
 	}
 
 	async function imageDelete<T>(image, idx) {
 		await deleteDoc(doc(db, 'viewers', $dbUser?.uid, 'images', image.id)).then(() => {
-			console.log(`LOG..+page: deleted`)
 			getImages()
 		})
 	}
 
 	async function parameter(image: Image) {
-		console.log(`LOG..+page: parameter`, image.carousel)
 		if (image.now) {
 			const { id } = image
 			images.forEach((img, idx) => {
@@ -109,6 +102,37 @@
 	onMount(async () => {})
 
 	$: if ($dbUser?.id) setup()
+
+	$: {
+		const regex = new RegExp(
+			/^(ftp|http|https|chrome|:\/\/|\.|@){2,}(localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\S*:\w*@)*([a-zA-Z]|(\d{1,3}|\.){7}){1,}(\w|\.{2,}|\.[a-zA-Z]{2,3}|\/|\?|&|:\d|@|=|\/|\(.*\)|#|-|%)*$/gmu,
+		)
+		let item: Image = {
+			url: newImg.url,
+			carousel: false,
+			gallery: false,
+			now: false,
+			title: '',
+		}
+
+		urlValid = regex.test(item.url)
+	}
+
+	$: {
+		manyFiltered = many
+			.split('\n')
+			.filter(item => item.length > 0)
+			.map(item => {
+				const [url, title] = item.split(',')
+				return {
+					url,
+					title: title?.trim() || '',
+					carousel: false,
+					gallery: false,
+					now: false,
+				}
+			})
+	}
 
 	// $: console.log(`LOG..+page: show`, show)
 </script>
@@ -175,7 +199,7 @@
 		<div class="w-full rounded-t-none shadow-xl card bg-neutral">
 			<div class="card-body">
 				{#if tab === 0}
-					<div class="flex items-center justify-start gap-2">
+					<div class="flex items-center justify-start gap-2" in:fly>
 						<input
 							type="title"
 							placeholder="Title (optional)"
@@ -187,7 +211,6 @@
 							placeholder="Image URL"
 							class="w-full max-w-xs input-sm input input-bordered input-neutral"
 							bind:value={newImg.url}
-							on:change={handleInputChange}
 						/>
 
 						<button
@@ -195,41 +218,38 @@
 							disabled={!urlValid}
 							on:click={() => addOne(newImg)}
 						>
-							Add
+							Add One
 						</button>
 					</div>
 				{:else if tab === 1}
-					<!--  -->
+					<div class="flex flex-col gap-4" in:fly>
+						<textarea
+							rows="5"
+							class="textarea textarea-bordered"
+							placeholder="Enter a list, one per line.
+URL, Title (optional)"
+							bind:value={many}
+						/>
+						<button
+							class="self-end btn btn-primary btn-sm"
+							disabled={manyFiltered.length === 0}
+							on:click={addMany}
+						>
+							Add Many
+						</button>
+					</div>
 				{:else if tab === 2}
 					<!--  -->
 				{/if}
 			</div>
 		</div>
 	</div>
-	<div class="flex items-center justify-start gap-2">
-		<input
-			type="title"
-			placeholder="Title (optional)"
-			class="w-full max-w-xs input-sm input input-bordered input-neutral"
-			bind:value={newImg.title}
-		/>
-		<input
-			type="url"
-			placeholder="Image URL"
-			class="w-full max-w-xs input-sm input input-bordered input-neutral"
-			bind:value={newImg.url}
-			on:change={handleInputChange}
-		/>
-
-		<button class="btn btn-primary btn-sm" disabled={!urlValid} on:click={() => addOne(newImg)}>
-			Add
-		</button>
-	</div>
 	<div class="flex justify-start">
 		<div class="form-control">
 			<label class="items-center gap-2 cursor-pointer label">
+				<span class="label-text"> List</span>
 				<input type="checkbox" class="toggle toggle-primary" bind:checked={tiles} />
-				<span class="label-text">Show {tiles ? 'Thumbs' : 'List'}</span>
+				<span class="label-text">Thumbs</span>
 			</label>
 		</div>
 	</div>
@@ -300,14 +320,14 @@
 								<img
 									src={image.url}
 									alt="image"
-									class="object-cover object-top w-12 h-12 rounded-2xl"
+									class="object-cover object-top w-8 h-8 rounded"
 								/>
 								<p class="w-28 font-md">{image.title || ''}</p>
 							</a>
 							<!-- <div class="flex flex-col gap-2"> -->
 
 							<button
-								class="text-gray-800 btn btn-sm"
+								class="text-gray-800 btn btn-xs font-xs"
 								class:unselected={!image.carousel}
 								class:btn-primary={image.carousel}
 								on:click={() => parameter({ ...image, carousel: !image.carousel })}
@@ -316,7 +336,7 @@
 							</button>
 
 							<button
-								class="text-gray-800 btn btn-sm"
+								class="text-gray-800 btn btn-xs font-xs"
 								class:unselected={!image.gallery}
 								class:btn-secondary={image.gallery}
 								on:click={() => parameter({ ...image, gallery: !image.gallery })}
@@ -325,7 +345,7 @@
 							</button>
 
 							<button
-								class="text-gray-800 btn btn-sm"
+								class="text-gray-800 btn btn-xs font-xs"
 								class:unselected={!image.now}
 								class:btn-accent={image.now}
 								on:click={() => parameter({ ...image, now: !image.now })}
