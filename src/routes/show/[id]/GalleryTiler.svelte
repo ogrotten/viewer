9be7ebd8967d02c78870d6e5226ab72044f87550
Darrin Lowery <darrin@buildlab.co>
2024-01-05 15:24:56 -0600

    you're dead to me.

--- src/routes/show/[id]/GalleryTiler.svelte
+++ src/routes/show/[id]/GalleryTiler.svelte
@@ -2,6 +2,7 @@
 	import { browser } from '$app/environment'
 	import { onMount, createEventDispatcher } from 'svelte'
 	import { page } from '$app/stores'
+	import type { Packery } from 'packery'
 	// import Packery from 'packery'
 
 	const uid = $page.params.id
@@ -11,7 +12,7 @@
 	export let presentGallery: Image[] = []
 	export let packWrap: HTMLSpanElement
 
-	let Packery, pk
+	let Packery, pk: Packery
 	onMount(async () => {
 		const module = await import('packery')
 		Packery = module.default
@@ -21,20 +22,47 @@
 			percentPosition: true,
 		})
 	})
+	// $: console.log(`LOG..GalleryTiler: pk`, pk?.getItemElements().at(-1))
 
-	$: if (pk) {
-		console.log(`LOG..GalleryTiler: pk`, pk.getItemElements()[0].naturalWidth)
+	let images = []
+	$: if (pk && gallery) {
+		// const newE = document.createElement('img')
+		// newE.src = gallery[8].url
+		// newE.id = 'brickitem'
+		// newE.classList.add('z-0', 'w-48', 'h-48')
+		// presentGallery = [...presentGallery, gallery[8]]
+
+		pk.appended(images)
+		debugger
+	}
+	const addOne = () => {
+		const newE = document.createElement('img')
+		newE.src = gallery[8].url
+		newE.id = 'brickitem'
+		newE.classList.add('z-0', 'w-48', 'h-48')
+		// presentGallery = [...presentGallery, gallery[8]]
+		pk.appended(newE)
+		pk.layout()
+		// debugger
+		console.log(`LOG..GalleryTiler: `, pk?.getItemElements().at(-1))
 	}
 
-	// on:click={()=> dispatch('imageClick', img)}
+	$: if (gallery) pk?.layout()
 </script>
 
-<div class="" bind:this={packWrap} id="packWrap">
-	{#each presentGallery as img, idx (img.id)}
-		<div class="relative">
-			<img src={img.url} alt="" class="z-0 w-48 h-48" id="brickitem" />
-		</div>
+<div class="hidden">
+	{#each presentGallery as img, idx (idx)}
+		<img bind:this={images[idx]} src={img.url} alt="" class="z-0 w-48 h-48" id="brickitem" />
 	{/each}
+</div>
+
+<button class=" btn-primary" on:click={addOne}>addOne</button>
+<div class="" bind:this={packWrap} id="packWrap">
+	<!-- {#each presentGallery as img, idx (idx)}
+		<div class="relative">
+		<img src={img.url} alt="" class="z-0 w-48 h-48" id="brickitem" />
+		</div>
+	{/each} -->
 </div>
 
 <style>
